3e6bf790b21bdae75b2d356e847b5f2baa918496
Shinzo SAITO <architshin@websarva.com>
2019-02-18 00:48:40 +0900

    第2回サンプルの追加。

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,24 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
+
+//第2回サンプル
+Route::get("/hello", function() {
+	return "<h1>Hello World!</h1>";
+});
+Route::get("/whoAreYou/{name}", function($name) {
+	return "<h1>こんにちは".$name."さん</h1>";
+});
+Route::get("/whoAreYouFull/{nameFirst}/{nameLast}",
+	function($nameFirst, $nameLast) {
+		return "<h1>こんにちは".$nameFirst." ".$nameLast."さん</h1>";
+	}
+);
+Route::get("/whoAreYouPart/{nameFirst}/{nameLast?}",
+	function($nameFirst, $nameLast = "") {
+		return "<h1>こんにちは".$nameFirst." ".$nameLast."さん</h1>";
+	}
+);
+Route::redirect("/google", "https://www.google.com/");
