d33975f44b0b6020ea11ba460cba356b3567bb62
Shinzo SAITO <architshin@websarva.com>
2020-02-14 20:31:36 +0900

    FileAccessExceptionのメソッドに型を記述。

--- app/Exceptions/FileAccessException.php
+++ app/Exceptions/FileAccessException.php
@@ -2,16 +2,18 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class FileAccessException extends Exception
 {
-	public function report()
+	public function report(): void
 	{
 		$errorMsg = $this->getMessage();
 		print("<h1>".$errorMsg."</h1>");
 	}
 	
-	public function render($request)
+	public function render(Request $request): Response
 	{
 		$data["errorMsg"] = $this->getMessage();
 		return response()->view("errors.custom", $data);
